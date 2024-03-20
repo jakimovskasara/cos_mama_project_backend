d270e7f0d9c7d745d88543fe9678d1b320e98013
Jakimovska Sara <110570071+jakimovskasara@users.noreply.github.com>
2024-03-20 15:19:28 +0200

    Update UserRepo.java
    - Added a new method getUserById(Long id) to retrieve a User object by its ID.
     - Added a new method updateUser(Long id, User user) to update the User object with the specified ID with the new user information.
    - Added a new method deleteUserById(Long id) to delete the User object with the specified ID.

--- src/main/java/cos_mama_backend/project/UserRepo.java
+++ src/main/java/cos_mama_backend/project/UserRepo.java
@@ -6,4 +6,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    User getUserById(Long id);
+
+    User updateUser(Long id, User user);
+
+    void deleteUserById(Long id);
 }
