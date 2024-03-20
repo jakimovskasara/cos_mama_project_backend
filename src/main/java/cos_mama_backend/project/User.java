f8bb137ff8605e15f3a8f3e71e52326dd90a5089
Jakimovska Sara <110570071+jakimovskasara@users.noreply.github.com>
2024-03-20 15:04:32 +0200

    Update User.java
    Added the "Forgot Password" functionality and made changes so the user is able to edit their personal data.

--- src/main/java/cos_mama_backend/project/User.java
+++ src/main/java/cos_mama_backend/project/User.java
@@ -1,10 +1,8 @@
 package cos_mama_backend.project;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class User {
@@ -25,6 +23,21 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = true)
+    private String phone;
+
+    @Column(nullable = true)
+    private String address;
+
+    @Column(nullable = true)
+    private String postalCode;
+
+    @Column(nullable = true)
+    private LocalDateTime forgotPasswordExpiration;
+
+    @Column(nullable = true)
+    private String forgotPasswordToken;
+
     public User() {}
 
     public User(String firstName, String lastName, String email, String password) {
@@ -34,12 +47,21 @@ public class User {
         this.password = password;
     }
 
-    // getters and setters
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName;
+    // forgot password functionality
+    public void forgotPassword(String token, LocalDateTime expiration) {
+        this.forgotPasswordToken = token;
+        this.forgotPasswordExpiration = expiration;
     }
+
+    public void resetPassword(String newPassword) {
+        this.password = newPassword;
+        this.forgotPasswordToken = null;
+        this.forgotPasswordExpiration = null;
+    }
+
+    // edit user information
+    public void editUserInfo(String firstName, String lastName, String email, String phone, String address, String postalCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
