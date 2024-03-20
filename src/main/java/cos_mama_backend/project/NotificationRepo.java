daadcc59886cc30d8f576af392af14c2cc66cf83
Jakimovska Sara <110570071+jakimovskasara@users.noreply.github.com>
2024-03-20 15:58:11 +0200

    Delete src/main/java/cos_mama_backend/project/NotificationRepo.java
    Not needed here.

--- src/main/java/cos_mama_backend/project/NotificationRepo.java
+++
@@ -1,9 +0,0 @@
-package cos_mama_backend.project;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepo extends JpaRepository<Notification, Long> {
-    Notification findByUserId(Long userId);
-}
