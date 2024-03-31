d121b731b365500e4de3a1868b702c2ca33cfae5
Jakimovska Sara <110570071+jakimovskasara@users.noreply.github.com>
2024-03-31 17:17:03 +0300

    Update Event.java
    Organiser and Category not needed

--- src/main/java/cos_mama_backend/project/Event.java
+++ src/main/java/cos_mama_backend/project/Event.java
@@ -9,19 +9,16 @@ public class Event {
     private LocalDate startDate;
     private LocalDate endDate;
     private String location;
-    private String organizer;
-    private String category;
+    
 
     // Getters and setters for all fields
 
-    public Event(String name, String description, LocalDate startDate, LocalDate endDate, String location, String organizer, String category) {
+    public Event(String name, String description, LocalDate startDate, LocalDate endDate, String location) {
         this.name = name;
         this.description = description;
         this.startDate = startDate;
         this.endDate = endDate;
         this.location = location;
-        this.organizer = organizer;
-        this.category = category;
     }
 
     // Add any additional methods here
